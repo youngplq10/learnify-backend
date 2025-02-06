13d8334dbf9c638b60a1f3baab289892615dbafe
youngplq10 <patryk.plq@o2.pl>
2025-02-06 18:21:05 +0100

    Initial commit

---
+++ src/main/java/dev/starzynski/learnify/LearnifyApplication.java
@@ -0,0 +1,13 @@
+package dev.starzynski.learnify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnifyApplication.class, args);
+	}
+
+}
